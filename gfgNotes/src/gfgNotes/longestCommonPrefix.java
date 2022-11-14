1d35d8cb87e5ae01cf4e133620ca96b011ec6eaa
kumkum <kumkumrathour00@gmail.com>
2022-11-14 15:41:42 +0530

    add

---
+++ gfgNotes/src/gfgNotes/longestCommonPrefix.java
@@ -0,0 +1,15 @@
+package gfgNotes;
+
+public class longestCommonPrefix {
+    StringBuilder s= new StringBuilder();
+        // Arrays.sort(strs);
+        // char[] first = strs[0].toCharArrays();
+        // char[] last = strs[strs.length-1].toCharArrays();
+        // for(int i = 0; i<first.length ; i++){
+        //     if(first[i] != last[i])
+        //     break;
+        //     s.append(first[i]);
+        // }
+        // return s.toString();
+    //  leedcode 14 question
+}
